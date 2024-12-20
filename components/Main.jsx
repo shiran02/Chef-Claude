34eb9a6b531a7f93ce35093c15397d3da458e5c3
Shiran Kumarasingha <shirankumarasingha97@gmail.com>
2024-12-20 19:39:46 +0530

    sf

--- components/Main.jsx
+++ components/Main.jsx
@@ -3,7 +3,7 @@ import React from "react"
 
 export default function Main(){
 
-    const [ingredients,setIngredients] = React.useState(["Chicken","Oregano","Tomatoes"]);
+    const [ingredients,setIngredients] = React.useState([]);
 
    
 
@@ -13,24 +13,35 @@ export default function Main(){
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function handleSubmit (event){
-        event.preventDefault()
-        console.log('Form sumbited')
-        const formData = new FormData(event.currentTarget)
-        const newIngredient = formData.get("ingredient")
-        // ingredients.push(newIngredient)
-        // console.log(ingredients)
+    // function handleSubmit (event){
+    //     event.preventDefault()
+    //     console.log('Form sumbited')
+    //     const formData = new FormData(event.currentTarget)
+    //     const newIngredient = formData.get("ingredient")
+    //     // ingredients.push(newIngredient)
+    //     // console.log(ingredients)
+
+    //     setIngredients( prevIngredient=> [
+    //         ...prevIngredient,
+    //         newIngredient
+    //     ])
+    // }
+
+
+    function addIngredient(formData){
+        const ingredients = formData.get('ingredient');
+        console.log(ingredients);
 
         setIngredients( prevIngredient=> [
             ...prevIngredient,
-            newIngredient
+            ingredients
         ])
     }
 
     
     return (
         <main>
-            <form className="add-ingredient-form" onSubmit={handleSubmit}>
+            <form className="add-ingredient-form" action={addIngredient}>
                 <input
                     type="text"
                     placeholder="e.g. oregano"
@@ -40,9 +51,18 @@ export default function Main(){
                 <button>Add ingredient</button>
             </form>
 
-            <ul>
-                {ingredientsListIems}
-            </ul>
+            { ingredients.length > 0 ? <section>
+                <h2>Ingredient on Hand</h2>
+                <ul className="ingredient-list"  aria-live="polite">{ingredientsListIems}</ul>
+
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section> : null}
             
         </main>
     )
