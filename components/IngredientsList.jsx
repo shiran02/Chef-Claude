57d047685d000ceb6f5d05f1660b0d1e6dd35a7d
Shiran Kumarasingha <shirankumarasingha97@gmail.com>
2024-12-26 18:02:21 +0530

    finish

---
+++ components/IngredientsList.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+export default function IngredientsList(props){
+
+    const ingredientsListIems = props.ingredients.map(ingredient=>(
+        <li key={ingredient}>{ingredient}</li>
+    ))
+    
+    return (
+        <section>
+            <h2>Ingredient on Hand</h2>
+            <ul className="ingredient-list"  aria-live="polite">{ingredientsListIems}</ul>
+
+            { props.ingredients.length > 3 &&
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button onClick={props.toggleRecipeShown}>Get a recipe</button>
+                </div> 
+                    
+            }
+        </section> 
+    )
+}
